d09d36058ba5922de360c352b0c7abd6c7e9efc0
Aliona <6066188@gmail.com>
2023-10-05 15:15:29 +0300

    Added written program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿string[] array1 = new string[6] {"7", "GeekBrains", "oh", ":-)", "Summer","23"};
+string[] array2 = new string[array1.Length];
+int j = 0;
+void SecondArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
